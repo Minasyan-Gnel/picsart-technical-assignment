f20507ca85c8e5844905a69b14ea39d81975b3f5
gminasyan <gminasyan@adobe.com>
2025-01-20 03:33:30 +0400

    feat: add Loader and NoResult in photo details page

--- src/components/PhotoDetails/PhotoDetails.tsx
+++ src/components/PhotoDetails/PhotoDetails.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 
+import { Loader } from '../Loader';
 import { Photo } from '../../types';
+import { NoResult } from '../NoResult';
 import { fetchPhotoById } from '../../api';
 
 import {
@@ -18,37 +20,49 @@ export const PhotoDetails = () => {
   const { id } = useParams();
 
   const [photo, setPhoto] = useState<Photo | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { width, height } = useMemo(() => {
+    if (!photo) return { width: 0, height: 0 };
+
+    const maxWidth = 1200;
+    const width = Math.min(photo.width, maxWidth);
+    const aspectRatio = photo.height / photo.width;
+    const height = width * aspectRatio;
+
+    return { width, height };
+  }, [photo]);
 
   useEffect(() => {
-    fetchPhotoById(Number(id)).then(setPhoto);
+    setIsLoading(true);
+    fetchPhotoById(Number(id)).then(setPhoto).finally(() => setIsLoading(false));
   }, [id]);
 
-  if (!photo) return null;
-
-  const maxWidth = 1200;
-  const width = Math.min(photo.width, maxWidth);
-  const aspectRatio = photo.height / photo.width;
-  const height = width * aspectRatio;
-
-  return <> 
+  return <>
     <PhotoDetailsHeaderStyled>
       <BackButtonStyled>
         <Link to="/">Go Back</Link>
       </BackButtonStyled>
     </PhotoDetailsHeaderStyled>
-    <PhotoDetailsContainerStyled>
-      <PhotoDetailsStyled>
-        <PhotoDetailsImageInfoStyled>
-          <PhotoDetailsInfoStyled>
-            <p><b>Author:</b> {photo.photographer}</p>
-            <p><b>Resolution:</b> {photo.width} x {photo.height}</p>
-            {photo.alt && <p><b>Description:</b> {photo.alt}</p>}
-          </PhotoDetailsInfoStyled>
-        </PhotoDetailsImageInfoStyled>
-        <PhotoDetailsImageWrapperStyled width={width} height={height}>
-          <img src={photo.src.original} alt={photo.alt || 'Original image'} />
-        </PhotoDetailsImageWrapperStyled>
-      </PhotoDetailsStyled>
-    </PhotoDetailsContainerStyled>
+    {isLoading ? (
+      <Loader />
+    ) : photo ? (
+      <PhotoDetailsContainerStyled>
+        <PhotoDetailsStyled>
+          <PhotoDetailsImageInfoStyled>
+            <PhotoDetailsInfoStyled>
+              <p><b>Author:</b> {photo.photographer}</p>
+              <p><b>Resolution:</b> {photo.width} x {photo.height}</p>
+              {photo.alt && <p><b>Description:</b> {photo.alt}</p>}
+            </PhotoDetailsInfoStyled>
+          </PhotoDetailsImageInfoStyled>
+          <PhotoDetailsImageWrapperStyled width={width} height={height}>
+            <img src={photo.src.original} alt={photo.alt || 'Original image'} />
+          </PhotoDetailsImageWrapperStyled>
+        </PhotoDetailsStyled>
+      </PhotoDetailsContainerStyled>
+    ) : (
+      <NoResult />
+    )}
   </>;
 };
